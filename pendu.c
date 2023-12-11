ca5c0704ae25e542e01a0dfc6275722ceac423d6
Emeric Krause <ekrause@k1r3p6.42mulhouse.fr>
2023-12-11 13:32:54 +0100

    welcome

--- pendu.c
+++ pendu.c
@@ -2,9 +2,11 @@
 
 void welcome()
 {
-	printf("╔════════════════╗
-║ hi    ║
-╚════════════════╝")
+	printf("Welcome to the hangman game\n");
+	sleep(1);
+	printf("Try to guess the secret word by entering letters one by one on the keyboard\n");
+	sleep(1);
+	pritnf("Be careful, the game is case sensitive\n");
 }
 
 int main()
